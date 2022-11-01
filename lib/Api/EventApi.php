e56ea452dc91e75c7783a265869d6edbdb4cc041
Volodymyr <volodymyr@hryvinskyi.com>
2022-11-01 18:14:33 +0200

    Updated swager code

--- lib/Api/EventApi.php
+++ lib/Api/EventApi.php
@@ -75,7 +75,7 @@ class EventApi
         HeaderSelector $selector = null
     ) {
         $this->client = $client ?: new Client();
-        $this->config = $config ?: new Configuration();
+        $this->config = $config ?: Configuration::getDefaultConfiguration();
         $this->headerSelector = $selector ?: new HeaderSelector();
     }
 
--- lib/Api/WebhookApi.php
+++ lib/Api/WebhookApi.php
@@ -75,7 +75,7 @@ class WebhookApi
         HeaderSelector $selector = null
     ) {
         $this->client = $client ?: new Client();
-        $this->config = $config ?: new Configuration();
+        $this->config = $config ?: Configuration::getDefaultConfiguration();
         $this->headerSelector = $selector ?: new HeaderSelector();
     }
 
